b8abc29caabbf6e0207df7a288c950df9e45106a
Monica <Monicasjensen@hotmail.com>
2021-06-22 16:29:05 +0200

    Update scripts.js
    At slideren kan auto play og adder den til at være responsiv

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,3 +1,18 @@
+const responsive = {
+    320:{
+        items: 1
+    },
+    560:{
+        items: 2
+    },
+    760:{
+        items: 3
+    },
+    960:{
+        items: 4
+    }
+}
+
 $(document).ready(function() {
 
     $nav = $('nav');
@@ -13,11 +28,12 @@ $(document).ready(function() {
 // owl-carousel for blog
 $('.owl-carousel').owlCarousel({
     loop: true,
-    autoplay: false,
+    autoplay: true,
     autoplayTimeout: 4000,
     dots: false,
     nav: true,
-    navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+    navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+    responsive: responsive
 });
 
 });
